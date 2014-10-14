4e91c3f521534f9c43cee7b6bd738e22b46fedc3
jaypeagi <git@average-joe.info>
2014-10-14 13:26:31 +0100

    Layout correct

--- script.js
+++ script.js
@@ -1,5 +1,68 @@
-//list all the quotes as a test
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+function getRandomArrayItem(array) {
+    var idx = getRandomInt(0,array.length);
+    return array[idx];
+}
+
+var numQs = 3;
+var numAs = 3;
+
 var $quotes = $("#quotes");
-$.each(quotes, function (i, e) {
-    $quotes.append("<blockquote>" + e.quote + "<cite>" + e.source + "</cite></blockquote>");
-});
+
+//$.each(quotes, function (i, e) {
+//    $quotes.append("<blockquote>" + e.quote + "<cite>" + e.source + "</cite></blockquote>");
+//});
+
+var usingQuotes = [];
+
+for (var i = 0; i < numQs; i++) {
+    var quote = getRandomArrayItem(quotes);
+    
+    //Make sure we haven't already used this one!
+    while(usingQuotes.indexOf(quote) != -1) {
+        quote = getRandomArrayItem(quotes);
+    }
+    
+    usingQuotes.push(quote);
+    
+    var elem = $("<li></li>").data("quote", quote);
+
+    if(i == 0) {
+        elem.addClass("current");
+    }
+    
+    elem.append("<blockquote>" + quote.quote + "</blockquote><ul class=\"answers\"></ul>");
+    
+    var answers = [ quote.source ];
+    
+    for (var i = 0; i < (numAs - 1); i++) {
+        var ans = getRandomArrayItem(sources);
+        
+        while(answers.indexOf(ans) != -1) {
+            ans = getRandomArrayItem(sources);
+        }
+        
+        answers.push(ans);
+    }
+    
+    var offset = getRandomInt(0,numAs);
+    var $answers = $(".answers", elem);
+    
+    for (var i = 0; i < numAs; i++) {
+        var idx = i + offset;
+        if(idx > answers.length - 1) {
+            idx -= answers.length;
+        }
+        
+        $answers.append("<li>" + answers[idx] +  "</li>");
+    }
+    
+    $quotes.append(elem);
+}
+
